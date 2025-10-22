24ad814bc89d1f5ba73e7f103806b04e13e485c1
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-21 21:00:24 -0600

    feat: add audit log model

---
+++ src/Domain.Endpoint/entities/auditLog.model.ts
@@ -0,0 +1,37 @@
+import { Action } from "./action.enum";
+import BaseModel from "./base.model";
+
+export default class AuditLog extends BaseModel {
+  entity: string; // User, Patient, Appointment, InventoryItem
+  entityId: string;
+  action: Action;
+  changes: JSON; // Detalle de cambios
+  performedBy: string;
+  performedAt: Date;
+
+  constructor({
+    id,
+    entity,
+    entityId,
+    action,
+    changes,
+    performedAt,
+    performedBy,
+  }: {
+    id: string;
+    entity: string;
+    entityId: string;
+    action: Action;
+    changes: JSON;
+    performedAt: Date;
+    performedBy: string;
+  }) {
+    super(id);
+    this.entity = entity;
+    this.entityId = entityId;
+    this.action = action;
+    this.changes = changes;
+    this.performedAt = performedAt;
+    this.performedBy = performedBy;
+  }
+}
