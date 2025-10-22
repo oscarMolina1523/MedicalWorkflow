4a27d7d405f4d607d1aac9d0dda9451044bc9053
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-22 13:31:50 -0600

    feat: add token service to generate dynamic tokens

--- src/Infrastructure.Endpoint/services/tokenService.ts
+++ src/Infrastructure.Endpoint/services/tokenService.ts
@@ -1,4 +1,3 @@
-import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { UserRequest } from "../../Aplication.Endpoint/dtos/request/user.request";
@@ -11,19 +10,4 @@ export function generateAccesToken(user: UserRequest) {
     throw new Error("JWT SECRET is not defined in environment variables");
 
   return jwt.sign(user, secret, { expiresIn: "1d" });
-}
-
-export function decodeToken(req: Request): UserRequest {
-  const secret = process.env.SECRET;
-  if (!secret) throw new Error("JWT SECRET is not defined");
-
-  const authHeader = req.headers["authorization"];
-  if (!authHeader) throw new Error("No token provided");
-
-  const token = authHeader.startsWith("Bearer ")
-    ? authHeader.slice(7)
-    : authHeader;
-
-  const decoded = jwt.verify(token, secret) as UserRequest;
-  return decoded;
 }
