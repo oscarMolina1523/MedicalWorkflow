d0869b2e3d42a950c45b0001fab29d591d8416cc
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-23 13:47:30 -0600

    feat: add entities type

---
+++ src/Infrastructure.Endpoint/utils/entityTypes.ts
@@ -0,0 +1,14 @@
+export enum EntityType{
+    User="User",
+    Role="Role",
+    Medication="Medication",
+    Department="Department",
+    Patient="Patient",
+    Appointment="Appointment",
+    Inventory="Inventory",
+    KPI="KPI",
+    AuditLog="AuditLog",
+    MedicalService="MedicalService",
+    Billing="Billing",
+    Expense="Expense"
+}
