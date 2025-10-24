7775ff04c3f2b9840163006a4d4ad9d45276802f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-23 18:00:22 -0600

    feat: add current user id field

--- src/Aplication.Endpoint/interfaces/userService.interface.ts
+++ src/Aplication.Endpoint/interfaces/userService.interface.ts
@@ -9,7 +9,7 @@ export interface IUserService {
   getById(id: string): Promise<UserResponse | null>;
   getByEmail(email: string): Promise<User | null>;
   getByAreaId(areaId: string): Promise<UserResponse[]>;
-  addUser(user: UserRequest): Promise<ServiceResult<UserResponse>>;
-  updateUser(id: string, user: UserRequest): Promise<ServiceResult<UserResponse | null>>;
+  addUser(user: UserRequest, currentUserId:string): Promise<ServiceResult<UserResponse>>;
+  updateUser(id: string, user: UserRequest, currentUserId: string): Promise<ServiceResult<UserResponse | null>>;
   deleteUser(id: string): Promise<{ success: boolean; message: string }>;
 }
