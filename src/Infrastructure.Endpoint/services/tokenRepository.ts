3cd9106521a6a653648f416f618e9efdeed7a541
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-25 17:15:38 -0600

    feat: add logic to return a decoded token

--- src/Infrastructure.Endpoint/services/tokenRepository.ts
+++ src/Infrastructure.Endpoint/services/tokenRepository.ts
@@ -7,11 +7,26 @@ dotenv.config();
 
 @injectable()
 export default class TokenRepository implements ITokenRepository {
-  generateAccesToken(user: UserResponse):string{
+  generateAccesToken(user: UserResponse): string {
     const secret = process.env.SECRET;
     if (!secret)
       throw new Error("JWT SECRET is not defined in environment variables");
 
     return jwt.sign(user, secret, { expiresIn: "1d" });
+  }
+
+  decodeToken(token: string): UserResponse {
+    const secret = process.env.SECRET;
+    if (!secret) throw new Error("JWT SECRET is not defined");
+
+    if (!token) throw new Error("No token provided");
+
+    // Manejar token con o sin 'Bearer ' al inicio
+    const cleanedToken = token.startsWith("Bearer ")
+      ? token.slice(7).trim()
+      : token.trim();
+
+    const decoded = jwt.verify(cleanedToken, secret) as UserResponse;
+    return decoded;
   }
 }
