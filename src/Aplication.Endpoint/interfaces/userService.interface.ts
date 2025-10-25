30dca2ac3b6af70944738fe56f3c4b082c7abe04
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-25 17:14:18 -0600

    refactor: use token to get data of session

--- src/Aplication.Endpoint/interfaces/userService.interface.ts
+++ src/Aplication.Endpoint/interfaces/userService.interface.ts
@@ -7,8 +7,8 @@ export interface IUserService {
   getUsers(): Promise<UserResponse[]>;
   getById(id: string): Promise<UserResponse | null>;
   getByEmail(email: string): Promise<User | null>;
-  getByAreaId(areaId: string): Promise<UserResponse[]>;
-  addUser(user: UserRequest, currentUserId:string): Promise<ServiceResult<UserResponse>>;
-  updateUser(id: string, user: UserRequest, currentUserId: string): Promise<ServiceResult<UserResponse | null>>;
+  getByAreaId(token: string): Promise<UserResponse[]>;
+  addUser(user: UserRequest, token:string): Promise<ServiceResult<UserResponse>>;
+  updateUser(id: string, user: UserRequest, token: string): Promise<ServiceResult<UserResponse | null>>;
   deleteUser(id: string): Promise<{ success: boolean; message: string }>;
 }
