9cf8c0b74b106611e83d316268a67267e26153b1
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-24 18:55:56 -0600

    feat: add user schema for scalar documentation

---
+++ src/WebApi/documentation/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+export const UserSchemas = {
+  UserRequest: {
+    type: "object",
+    required: ["email", "password", "departmentId", "active"],
+    properties: {
+      username: { type: "string" },
+      email: { type: "string" },
+      password: { type: "string" },
+      departmentId: { type: "string" },
+      roleId: { type: "string" },
+      active: { type: "boolean" },
+    },
+  },
+  UserResponse: {
+    type: "object",
+    properties: {
+      id: { type: "string" },
+      username: { type: "string" },
+      email: { type: "string" },
+      roleId: { type: "string" },
+      active: { type: "boolean" },
+      createdAt: { type: "string", format: "date-time" },
+      updatedAt: { type: "string", format: "date-time" },
+      createdBy: { type: "string" },
+      updatedBy: { type: "string" },
+      departmentId: { type: "string" },
+    },
+  },
+};
