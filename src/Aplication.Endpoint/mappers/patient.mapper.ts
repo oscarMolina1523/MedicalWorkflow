4b4913296ef69a825ef8f609df383bf8ad2d6814
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-28 09:52:01 -0600

    feat: add method to update patient

--- src/Aplication.Endpoint/mappers/patient.mapper.ts
+++ src/Aplication.Endpoint/mappers/patient.mapper.ts
@@ -20,4 +20,20 @@ export class PatientMapper {
       updatedBy: currentUser,
     });
   }
+
+  static updateEntity(existing: Patient, dto: PatientRequest, currentUser: string): Patient {
+    const now = new Date();
+
+    return {
+      ...existing, // mantiene id, createdAt, createdBy, etc.
+      firstName: dto.firstName.trim() ? dto.firstName: existing.firstName,
+      lastName: dto.lastName.trim() ? dto.lastName: existing.lastName,
+      birthDate: dto.birthDate ?? existing.birthDate,
+      gender: dto.gender.trim() ? dto.gender: existing.gender,
+      departmentId: dto.departmentId.trim() ? dto.departmentId: existing.departmentId,
+      medicalHistory: dto.medicalHistory.trim() ? dto.medicalHistory: existing.medicalHistory,
+      updatedAt: now,
+      updatedBy: currentUser,
+    };
+  }
 }
